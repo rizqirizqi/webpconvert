c307cef02893ecd12e20558aa6f08077110dbde5
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2019-12-17 14:13:31 +0700

    Fix directory options

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const yargs = require('yargs');
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const rename = require('gulp-rename');
-const webp = require("imagemin-webp");
+const webp = require('imagemin-webp');
+const path = require('path');
 
+const DEFAULT_SOURCE = '.';
+const DEFAULT_TARGET = 'same as source directory';
 const argv = yargs
   .usage('Convert jpg/png images in specified directory to webp\nUsage: webpconvert [options]')
   .example('webpconvert')
-  .example('webpconvert -s ./sourceImg -t ./targetImg')
+  .example('webpconvert -s sample-images')
+  .example('webpconvert -s sample-images -t sample-images')
   .options('source', {
     alias: 's',
     type: 'string',
     describe: 'source directory',
-    default: '.',
+    default: DEFAULT_SOURCE,
   })
   .options('target', {
     alias: 't',
     type: 'string',
     describe: 'target directory',
-    default: '.',
+    default: DEFAULT_TARGET,
   })
   .help()
   .alias('help', 'h')
@@ -27,10 +31,10 @@ const argv = yargs
   .alias('version', 'v')
   .argv;
 
-const sourceDir = argv.sourceDir || '.';
-const targetDir = argv.targetDir || '.';
-const PNGImages = `${sourceDir}/*.png`;
-const JPEGImages = `${sourceDir}/*.jpg`;
+const sourceDir = argv.source || '.';
+const targetDir = argv.target !== DEFAULT_TARGET ? argv.target : sourceDir;
+const PNGImages = path.resolve(sourceDir, '*.png');
+const JPEGImages = path.resolve(sourceDir, '*.jpg');
 
 gulp.src(PNGImages)
   .pipe(imagemin([webp({
@@ -39,10 +43,7 @@ gulp.src(PNGImages)
     verbose: true
   }))
   .pipe(rename({ suffix: ".png", extname: '.webp' }))
-  .pipe(gulp.dest(targetDir))
-  .on("finish", () => {
-    console.log('PNG Images optimized');
-  });
+  .pipe(gulp.dest(targetDir));
 
 gulp.src(JPEGImages)
   .pipe(imagemin([webp({
@@ -51,7 +52,4 @@ gulp.src(JPEGImages)
     verbose: true
   }))
   .pipe(rename({ suffix: ".jpg", extname: '.webp' }))
-  .pipe(gulp.dest(targetDir))
-  .on("finish", () => {
-    console.log('JPG Images optimized');
-  });
+  .pipe(gulp.dest(targetDir));
