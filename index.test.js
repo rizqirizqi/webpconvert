dd4dd4530db5473d59206c6158e51c95e13e2260
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2021-12-03 22:27:11 +0700

    Fix unittest

--- index.test.js
+++ index.test.js
@@ -52,9 +52,10 @@ const runCLI = (args = '', cwd = process.cwd()) => {
   });
 };
 
-test('webpconvert --help', () => runCLI('--help').then((stdout) => {
+test('webpconvert --help', async () => {
+  const stdout = await runCLI('--help');
   expect(stdout).toMatchSnapshot();
-}));
+});
 
 describe('Convert Images', () => {
   beforeEach(() => {
@@ -68,69 +69,65 @@ describe('Convert Images', () => {
     let outputImg = getOutputImages();
     expect(inputImg.children.length).toBe(4);
     expect(outputImg.children.length).toBe(0);
-    return runCLI('', SAMPLE_DIRECTORY).then((stdout) => {
-      outputImg = getOutputImages();
-      expect(outputImg.children.length).toBe(4);
-      expect(stdout.match(/Minified 2 images/g)).toHaveLength(2);
-    });
+    const stdout = await runCLI('', SAMPLE_DIRECTORY);
+    outputImg = getOutputImages();
+    expect(outputImg.children.length).toBe(4);
+    expect(stdout.match(/Minified 2 images/g)).toHaveLength(2);
   });
   test('webpconvert sample-images | it convert images in specified directory', async () => {
     const inputImg = getInputImages();
     let outputImg = getOutputImages();
     expect(inputImg.children.length).toBe(4);
     expect(outputImg.children.length).toBe(0);
-    return runCLI(SAMPLE_DIRECTORY).then((stdout) => {
-      outputImg = getOutputImages();
-      expect(outputImg.children.length).toBe(4);
-      expect(stdout.match(/Minified 2 images/g)).toHaveLength(2);
-    });
+    const stdout = await runCLI(SAMPLE_DIRECTORY);
+    outputImg = getOutputImages();
+    expect(outputImg.children.length).toBe(4);
+    expect(stdout.match(/Minified 2 images/g)).toHaveLength(2);
   });
   test('webpconvert sample-images output | it convert images from input directory to output directory', async () => {
     const inputImg = getInputImages();
     let outputImg = getOutputImages('output');
     expect(inputImg.children.length).toBe(4);
     expect(outputImg).toBe(null);
-    return runCLI(`${SAMPLE_DIRECTORY} output`).then((stdout) => {
+    try {
+      const stdout = await runCLI(`${SAMPLE_DIRECTORY} output`);
       outputImg = getOutputImages('output');
       expect(outputImg.children.length).toBe(4);
       expect(stdout.match(/Minified 2 images/g)).toHaveLength(2);
-    }).finally(() => {
+    } finally {
       clearOutputs('output');
       rmdirSync('output');
-    });
+    }
   });
   test('webpconvert sample-images/KittenJPG.jpg | it convert a single image file', async () => {
     const inputImg = getInputImages();
     let outputImg = getOutputImages();
     expect(inputImg.children.length).toBe(4);
     expect(outputImg.children.length).toBe(0);
-    return runCLI(`${SAMPLE_DIRECTORY}/KittenJPG.jpg`).then((stdout) => {
-      outputImg = getOutputImages();
-      expect(outputImg.children.length).toBe(1);
-      expect(stdout.match(/Minified 1 image/g)).toHaveLength(1);
-    });
+    const stdout = await runCLI(`${SAMPLE_DIRECTORY}/KittenJPG.jpg`);
+    outputImg = getOutputImages();
+    expect(outputImg.children.length).toBe(1);
+    expect(stdout.match(/Minified 1 image/g)).toHaveLength(1);
   });
   test('webpconvert sample-images/KittenPNG.jpg | it convert a single image file with quality 50', async () => {
     const inputImg = getInputImages();
     let outputImg = getOutputImages();
     expect(inputImg.children.length).toBe(4);
     expect(outputImg.children.length).toBe(0);
-    return runCLI(`${SAMPLE_DIRECTORY}/KittenPNG.png -q 50`).then((stdout) => {
-      outputImg = getOutputImages();
-      expect(outputImg.children.length).toBe(1);
-      expect(stdout.match(/Minified 1 image/g)).toHaveLength(1);
-    });
+    const stdout = await runCLI(`${SAMPLE_DIRECTORY}/KittenPNG.png -q 50`);
+    outputImg = getOutputImages();
+    expect(outputImg.children.length).toBe(1);
+    expect(stdout.match(/Minified 1 image/g)).toHaveLength(1);
   });
   test('webpconvert sample-images/KittenPNG.jpg | it convert a single image file with prefix and suffix', async () => {
     const inputImg = getInputImages();
     let outputImg = getOutputImages();
     expect(inputImg.children.length).toBe(4);
     expect(outputImg.children.length).toBe(0);
-    return runCLI(`${SAMPLE_DIRECTORY}/KittenPNG.png --prefix="img-" --suffix="-compressed"`).then((stdout) => {
-      outputImg = getOutputImages();
-      expect(outputImg.children.length).toBe(1);
-      expect(outputImg.children[0].name).toMatch('img-KittenPNG-compressed.png.webp');
-      expect(stdout.match(/Minified 1 image/g)).toHaveLength(1);
-    });
+    const stdout = await runCLI(`${SAMPLE_DIRECTORY}/KittenPNG.png --prefix="img-" --suffix="-compressed"`);
+    outputImg = getOutputImages();
+    expect(outputImg.children.length).toBe(1);
+    expect(outputImg.children[0].name).toMatch('img-KittenPNG-compressed.png.webp');
+    expect(stdout.match(/Minified 1 image/g)).toHaveLength(1);
   });
 });
