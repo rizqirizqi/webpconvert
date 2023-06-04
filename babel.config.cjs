8739111083f1e6b673c6110896a11ef19fcee8a8
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2023-06-04 16:36:40 +0700

    Upgrade deps and deprecate node 12 support

---
+++ babel.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
+  ],
+};
